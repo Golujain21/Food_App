f6fd88f41ee0b2424ea5662b116d5a1a58d24381
Golujain21 <golujain2109@gmail.com>
2024-02-01 16:47:34 +0530

    initial commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {
+  registerController,
+  loginController,
+} = require("../controllers/authControllers");
+
+//routes
+
+//REGISTER || POST
+//REGISTER || POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+
+router.post("/login", loginController);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,32 @@
+require("dotenv").config();
 const express = require("express");
-
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDb = require("./config/conn");
 // rest object
 const app = express();
+//DB connection
+connectDb();
+// middleWare
+app.use(cors());
+app.use(express.json());
+app.use(morgan());
 
 // route
+// URL => http://localhost:8080
+app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/resturant", require("./routes/resturantRoutes"));
+app.use("/api/v1/category", require("./routes/catgeoryRoutes"));
+
 app.get("/", (req, res) => {
   return res.status(200).send("<h1> welcome to food server <h1>");
 });
 
-const Port = 8080;
+const Port = process.env.PORT || 8080;
 
 app.listen(Port, () => {
-  console.log("server running");
+  console.log(`Server Running on mode on port ${Port}`.bgCyan.white);
 });
